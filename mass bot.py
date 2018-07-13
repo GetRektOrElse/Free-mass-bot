fb29f2510c90f86752106f55e3a37f1e8e87cb80
JustARatherRidiculouslyLongUsername <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2018-07-14 03:10:35 +0400

    Add mouse moving function
    The program now moves your mouse around the center of your screenn

--- mass bot.py
+++ mass bot.py
@@ -1,6 +1,13 @@
 from math import sin, cos, tan, asin, acos, atan, pi
-
+import pyautogui as gui
 
 def coords(center, radius, angle):
-    offset = (round(radius * sin(angle), 3), round(radius * cos(angle), 3))
+    offset = (round(radius * sin(angle), 3), -round(radius * cos(angle), 3))
     return tuple(a + b for a, b in zip(center, offset))
+
+def rotate(center, radius, seconds, points):
+    unit_angle = 2 * pi / points
+    for i in range(points):
+        gui.moveTo(*coords(center, radius, i * unit_angle), seconds/points)
+
+rotate([i/2 for i in gui.size()], 150, 0, 20)
