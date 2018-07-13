84a9cb8b576df7127f2f9823ca2aca2a838002db
JustARatherRidiculouslyLongUsername <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2018-07-14 01:44:35 +0400

    Remove unwanted line

--- mass bot.py
+++ mass bot.py
@@ -4,5 +4,3 @@ from math import sin, cos, tan, asin, acos, atan, pi
 def coords(center, radius, angle):
     offset = (round(radius * sin(angle), 3), round(radius * cos(angle), 3))
     return tuple(a + b for a, b in zip(center, offset))
-
-print(coords([5, 2], 2, pi*1.5))
